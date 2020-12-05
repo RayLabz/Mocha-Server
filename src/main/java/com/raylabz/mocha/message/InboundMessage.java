11cfd5e054840c02a0af4575c6cb5ac1508b6a94
Nicos <hfnovember@hotmail.com>
2020-12-05 13:54:32 +0200

    (v0.1.2.B5) InboundMessage & OutboundMessage, import from ByteSurge.

---
+++ src/main/java/com/raylabz/mocha/message/InboundMessage.java
@@ -0,0 +1,4 @@
+package com.raylabz.mocha.message;
+
+public class InboundMessage {
+}
--- src/test/java/MyServer.java
+++ src/test/java/MyServer.java
@@ -38,7 +38,7 @@ public class MyServer extends Server {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    System.out.println(i);
+//                    System.out.println(i);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
